073acb154126ca1614d98ee460c7ffbe8990ec0a
medlabidi <med.labidi.mohamed@gmail.com>
2026-01-06 17:31:22 +0100

    Trim CORS_ORIGIN to remove line breaks

--- api/index.ts
+++ api/index.ts
@@ -879,7 +879,7 @@ async function handleGetDefaultPrinter(req: VercelRequest, res: VercelResponse) 
 export default async (req: VercelRequest, res: VercelResponse) => {
   // Set CORS headers immediately
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN || '*');
+  res.setHeader('Access-Control-Allow-Origin', (process.env.CORS_ORIGIN || '*').trim());
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
 
